44895898e5e3c5e850071512c2bd6a204381e819
史凯明 <skm1992@163.com>
2017-07-06 23:53:33 +0800

    适配器

---
+++ 设计模式/1,适配器模式/1,适配器模式/1,适配器模式/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1,适配器模式
+//
+//  Created by kaiming shi on 2017/7/6.
+//  Copyright © 2017年 kaiming shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 设计模式/1,适配器模式/1,适配器模式/1,适配器模式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1,适配器模式
+//
+//  Created by kaiming shi on 2017/7/6.
+//  Copyright © 2017年 kaiming shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
